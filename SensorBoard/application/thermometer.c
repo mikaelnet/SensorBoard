f7294213bb76070043c53ddcfc5a5a44a07a3ad5
Mikael Hogberg <mikael.hogberg@stendahls.se>
2014-04-23 19:49:06 +0200

    Event driven temperature reading with support for multiple devices. (Untested code)

--- SensorBoard/application/thermometer.c
+++ SensorBoard/application/thermometer.c
@@ -18,6 +18,8 @@
 #include <avr/pgmspace.h>
 #include <stdio.h>
 
+#define MAX_DEVICES     4
+
 static Process_t thermometer_process;
 static CPU_SleepMethod_t sleep_methods;
 static OneWire_t oneWire;
@@ -25,51 +27,163 @@ static OneWire_t oneWire;
 static Terminal_Command_t command;
 static const char command_name[] PROGMEM = "TEMP";
 
-typedef enum thermometer_state_enum {
+/*typedef enum thermometer_state_enum {
     Idle,
     PowerUp,
     Reading,
 } thermometer_state_t;
-static thermometer_state_t _state;
+static thermometer_state_t _state;*/
 
+static void (*stateMethod)();
 static uint16_t _startTime;
-static void thermometer_get_temp3()
+
+typedef struct ThermometerDevice_struct {
+    uint8_t address[8];
+    struct ThermometerDevice_struct *next;
+} ThermometerDevice_t;
+static ThermometerDevice_t deviceArray[MAX_DEVICES];
+static uint8_t numDevices = 0;
+static uint8_t currentDevice = 0;
+
+static void thermometer_on_begin_read();
+
+static void thermometer_on_read_complete() {
+    if (cpu_millisecond() - _startTime < 750)
+        return;
+
+    uint16_t raw = DS1820_ReadTemperature (&oneWire, deviceArray[currentDevice].address);
+    char temperature[10];
+    snprintf_P(temperature, sizeof(temperature), PSTR(" %4d.%01d%cC"), raw >> 4, (raw << 12) / 6553, 0xB0);
+    printf_P(PSTR("\nTemperature: %s\n"), temperature);
+
+    currentDevice ++;
+    if (currentDevice < numDevices) {
+        currentDevice ++;
+        stateMethod = &thermometer_on_begin_read;
+    }
+    else
+        stateMethod = NULL;
+}
+
+static void thermometer_on_begin_read() {
+    printf_P(PSTR("Reading device %d..."), currentDevice+1);
+    
+    DS1820_StartConvertion (&oneWire, deviceArray[currentDevice].address);
+    
+    _startTime = cpu_millisecond();
+    stateMethod = &thermometer_on_read_complete;
+}
+
+static void thermometer_on_powerup() {
+    if (cpu_millisecond() - _startTime < 500)
+    return;
+    currentDevice = 0;
+    stateMethod = &thermometer_on_begin_read;
+}
+
+static void thermometer_get_temp()
 {
-    puts_P(PSTR("Reading temperature using method 2"));
-    if (_state != Idle) {
+    if (numDevices == 0) {
+        puts_P(PSTR("No devices. aborting."));
+        return;
+    }
+    
+    puts_P(PSTR("Reading temperature"));
+    if (stateMethod != NULL) {
         puts_P(PSTR("Incorrect state. aborting."));
         return;
     }
 
     thsen_enable();
     _startTime = cpu_millisecond();
-    _state = PowerUp;
+    stateMethod = &thermometer_on_powerup;
 }
 
-uint8_t fixedAddr[8] = { 0x28, 0x42, 0x3C, 0x28, 0x04, 0x00, 0x00, 0x52 };
-
-static void thermometer_on_powerup() {
-    if (cpu_millisecond() - _startTime < 500)
-        return;
-
-    puts_P(PSTR("Reading specific address..."));
-    DS1820_StartConvertion (&oneWire, fixedAddr);
-    _startTime = cpu_millisecond();
-    _state = Reading;
+static void thermometer_scan_devices()
+{
+    puts_P(PSTR("Scanning for DS18S20 devices"));
+    thsen_enable();
+    _delay_ms(500);
+    
+    //currentDevice = deviceArray[0];
+    numDevices = 0;
+    ThermometerDevice_t *dev = &deviceArray[0];
+    for (bool found = DS1820_FindFirst(&oneWire, dev->address) ; 
+        found && numDevices < MAX_DEVICES ; 
+        found = DS1820_FindNext(&oneWire, dev->address)) {
+        
+        dev->next = NULL;
+        if (numDevices > 0)
+            deviceArray[numDevices-1].next = dev;
+        
+        printf_P(PSTR("Device %d at "));
+        for (uint8_t i = 0 ; i < 8 ; i ++)
+            printf_P(PSTR(" %02X"), dev->address[i]);
+        printf_P(PSTR("\n"));
+        
+        numDevices ++;
+        dev = &deviceArray[numDevices];
+    }
 }
 
-static void thermometer_on_reading() {
-    if (cpu_millisecond() - _startTime < 750)
-        return;
-
-    uint16_t raw = DS1820_ReadTemperature (&oneWire, fixedAddr);
-    char temperature[10];
-    snprintf_P(temperature, sizeof(temperature), PSTR(" %4d.%01d%cC"), raw >> 4, (raw << 12) / 6553, 0xB0);
-    printf_P(PSTR("\nTemperature: %s\n"), temperature);
-
-    _state = Idle;
+static bool parse_command (const char *args)
+{
+    if (args == NULL || *args == 0 || strcasecmp_P(args, PSTR("GET")) == 0) {
+        thermometer_get_temp();
+        return true;
+    }
+    if (strcasecmp_P(args, PSTR("SCAN")) == 0) {
+        thermometer_scan_devices();
+        return true;
+    }
+    return false;
 }
 
+static void print_menu ()
+{
+    puts_P(PSTR("Get temperature from DS18B20 temp sensor"));
+}
+
+static void print_help ()
+{
+    puts_P(PSTR("GET   Read data"));
+    puts_P(PSTR("SCAN  Scan for devices"));
+}
+
+static void event_handler (EventArgs_t *args)
+{
+    if (args->senderId == DEVICE_CLOCK_ID && args->eventId == MINUTE) {
+        // Here we may read temperature...
+        thermometer_get_temp();
+    }
+}
+
+static void loop () {
+    if (stateMethod != NULL)
+        stateMethod();
+}
+
+static bool can_sleep() {
+    return stateMethod == NULL;
+}
+
+static void before_sleep() {
+    stateMethod = NULL;
+    thsen_disable();
+}
+
+void thermometer_init()
+{
+    OneWire_init(&oneWire, &PORTD, 5);
+    thermometer_scan_devices();
+
+    terminal_register_command(&command, command_name, &print_menu, &print_help, &parse_command);
+    cpu_register_sleep_methods(&sleep_methods, &can_sleep, &before_sleep, NULL);
+    process_register(&thermometer_process, &loop, &event_handler);
+}
+
+
+/*
 static void thermometer_get_temp2()
 {
     puts_P(PSTR("Reading temperature using method 2"));
@@ -175,72 +289,4 @@ static void thermometer_get_temp()
 
     thsen_disable();
 }
-
-static bool parse_command (const char *args)
-{
-    if (args == NULL || *args == 0 || strcasecmp_P(args, PSTR("GET")) == 0) {
-        thermometer_get_temp();
-        return true;
-    }
-    else if (strcasecmp_P(args, PSTR("GET2")) == 0) {
-        thermometer_get_temp2();
-        return true;
-    }
-    else if (strcasecmp_P(args, PSTR("GET3")) == 0) {
-        thermometer_get_temp3();
-        return true;
-    }
-    return false;
-}
-
-static void print_menu ()
-{
-    puts_P(PSTR("Get temperature from DS18B20 temp sensor"));
-}
-
-static void print_help ()
-{
-    puts_P(PSTR("GET   Read data"));
-}
-
-static void event_handler (EventArgs_t *args)
-{
-    if (args->senderId == DEVICE_CLOCK_ID && args->eventId == MINUTE) {
-        // Here we may read temperature...
-        thermometer_get_temp3();
-    }
-}
-
-static void loop () {
-    switch(_state) {
-        case PowerUp:
-            thermometer_on_powerup();
-            break;
-
-        case Reading:
-            thermometer_on_reading();
-            break;
-
-        default:
-            break;
-    }
-}
-
-static bool can_sleep() {
-    return _state == Idle;
-}
-
-static void before_sleep() {
-    _state = Idle;
-    thsen_disable();
-}
-
-void thermometer_init()
-{
-    OneWire_init(&oneWire, &PORTD, 5);
-
-    terminal_register_command(&command, command_name, &print_menu, &print_help, &parse_command);
-    cpu_register_sleep_methods(&sleep_methods, &can_sleep, &before_sleep, NULL);
-    process_register(&thermometer_process, &loop, &event_handler);
-}
-
+*/
